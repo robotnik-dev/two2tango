a034fb7df35a1e99867e821122df36560620836f
robotnik-dev <robotnik.games@gmail.com>
2025-05-30 16:56:02 +0200

    fix: update Android build commands and dependencies
    - Added OS-specific commands for building Android debug.
    - Updated dependencies in Cargo.lock and Cargo.toml.
    - Introduced clang_osx.sh for Android NDK configuration on mac OS

---
+++ rust/clang_osx.sh
@@ -0,0 +1,4 @@
+export CLANG_PATH=\
+"$HOME/Library/Android/sdk/ndk/23.2.8568313/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=\
+"$HOME/Library/Android/sdk/ndk/23.2.8568313/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android31-clang"
